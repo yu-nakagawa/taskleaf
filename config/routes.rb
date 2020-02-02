4fd755d2b5b824974a5e996479caee80bf5b931b
Yu Nakagawa <nakagawayu@nakagawahiroshinoMacBook-Pro.local>
2020-02-02 15:44:08 +0900

    Add System Spec

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
   namespace :admin do
     get 'users/new'
     get 'users/edit'
     get 'users/show'
     get 'users/index'
   end
-  get '/login', to: 'sessions#new'
-  post 'login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   namespace :admin do
     resources :users
--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -1,22 +1,64 @@
 require 'rails_helper'
 
 describe 'タスク管理機能', type: :system do
-  describe '一覧表示機能' do
-    before do
-      user_a = FactoryBot.create(:user, name: 'ユーザA', email: 'a@example.com')
-      FactoryBot.create(:task, name: '最初のタスク', user: user_a)
-    end
-    
-    context 'ユーザAがログインしている時' do
-      before do
-        visit login_path
-        fill_in 'メールアドレス', with: 'a@example.com'
-        fill_in 'パスワード', with: 'password'
-        click_button 'ログインする'
-      end
+  let(:user_a){FactoryBot.create(:user, name: 'ユーザA', email: 'a@example.com')}
+  let(:user_b){FactoryBot.create(:user, name: 'ユーザB', email: 'b@example.com')}
+  let!(:task_a){FactoryBot.create(:task, name: '最初のタスク', user: user_a)}
+  before do
+    visit login_path
+    first(".form-control").set(login_user.email)
+    page.all(".form-control")[1].set(login_user.password)
+    click_button 'ログインする'
+  end
 
-      it 'ユーザAが作成したタスクが表示される' do
-        expect(page).to have_content '最初のタスク'
+  shared_examples_for 'ユーザAが作成したタスクが表示される' do
+    it {expect(page).to have_content '最初のタスク'}
+  end
+  
+  describe '一覧表示機能' do
+    context 'ユーザAがログインしている時' do
+      let(:login_user){user_a}
+      it_behaves_like 'ユーザAが作成したタスクが表示される'
+    end
+    context 'ユーザBがログインしている時' do
+      let(:login_user){user_b}
+      it 'ユーザAが作成したタスクが表示されない' do
+        expect(page).to have_no_content '最初のタスク'
+      end
+    end
+  end
+
+  describe '詳細表示機能' do
+    context 'ユーザAがログインしている時' do
+      let(:login_user){user_a}
+
+      before do
+        visit task_path(task_a)
+      end
+      it_behaves_like 'ユーザAが作成したタスクが表示される'
+    end
+  end
+
+  describe '新規作成機能' do
+    let(:login_user){user_a}
+    before do
+      visit new_task_path
+      first(".form-control").set(task_name)
+      click_button '登録する'
+    end
+    context '新規作成画面で名称を入力した時' do
+      let(:task_name){'新規作成のテストを書く'}
+      it '正常に登録される' do
+        expect(page).to have_selector '.alert-success', text: '新規作成のテストを書く'
+      end
+    end
+
+    context '新規作成画面で名称を入力しなかった時' do
+      let(:task_name){''}
+      it 'エラーとなる' do
+        within '#error_explanation' do
+          expect(page).to have_content '名称を入力してください'
+        end
       end
     end
   end
